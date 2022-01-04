96f1f81f1a3c94123d0d98548e3f8133bbb53e25
oguzhan <oguzhanerkhrmn@gmail.com>
2022-01-04 04:19:54 +0300

    hazir sayilir

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AccountService } from './services/account.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'shop';
+  constructor(private accountService: AccountService, private router:Router){}
+
+  isLoggedIn(){
+    return this.accountService.isLoggedIn()
+  }
+  logOut(){
+    this.accountService.logout()
+    this.router.navigate(["products"])
+  }
+
 }
